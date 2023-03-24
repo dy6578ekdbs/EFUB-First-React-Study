4b3a5b62d5388fb6b1b8bd6ec072a4888fe017f2
ooherin <555ohr@naver.com>
2023-03-24 21:51:09 +0900

    1주차리액트

---
+++ day1/chapter_03/Book.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Book(props){
+    return(
+        <div>
+            <h1>{`이 책의 이름은 ${props.name}`}</h1>
+            <h2>{`이 책은 총 ${props.numOfPage}페이지로 이루어져 있습니다`}</h2>
+        </div>
+        );
+    }
+export default Book;
