feb12d0da0ee5020211fc7ce41432b512f289985
teyeong <100225783+teyeong@users.noreply.github.com>
2023-04-15 02:38:35 +0900

    리액트 스터디 4주차
    섹션 8 - 클릭 이벤트
    섹션 9 - 로그인 여부 툴바
    섹션 10 - 출석부 출력

---
+++ chapter_09/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Toolbar from "./Toolbar";
+
+function LandingPage(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const onClickLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    const onClickLogout = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <div>
+            <Toolbar
+                isLoggedIn={isLoggedIn}
+                onClickLogin={onClickLogin}
+                onClickLogout={onClickLogout}
+            />
+            <div style={{ padding: 16 }}>리액트 스터디 4주차 9장 실습!</div>
+        </div>
+    );
+}
+
+export default LandingPage;
