db56b34fddd1853dbc10eb31ec08e7217f5a238a
Jeongmin Cho <jojm0829@gmail.com>
2023-04-15 22:41:59 +0900

    chapter_09 로그인 여부를 나타내는 툴바 만들기

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 
 import Accommodate from "./chapter_07/Acommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>
 );
 
