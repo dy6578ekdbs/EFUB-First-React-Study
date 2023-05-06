e3da38ed9844a58818b0bfc81687174d2639efa6
Jeongmin Cho <jojm0829@gmail.com>
2023-05-06 22:33:48 +0900

    chapter_11_✨ feat: 성별 필드 추가

--- src/chapter_11/SignUp.jsx
+++ src/chapter_11/SignUp.jsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 
 function SignUp(props) {
   const [name, setName] = useState("");
+  const [gender, setGender] = useState("남자");
 
   const handleChangeName = (event) => {
     setName(event.target.value);
   };
+  const handleChangeGender = (event) => {
+    setGender(event.target.value);
+  };
 
   const handleSubmit = (event) => {
-    alert(`이름: ${name}`);
+    alert(`이름: ${name}, 성별: ${gender}`);
     event.preventDefault();
   };
 
@@ -17,8 +21,16 @@ function SignUp(props) {
       <label>
         이름:
         <input type="text" value={name} onChange={handleChangeName} />
-        <button type="submit">제출</button>
       </label>
+      <br />
+      <label>
+        성별:
+        <select value={gender} onChange={handleChangeGender}>
+          <option value="남자">남자</option>
+          <option value="여자">여자</option>
+        </select>
+      </label>
+      <button type="submit">제출</button>
     </form>
   );
 }
