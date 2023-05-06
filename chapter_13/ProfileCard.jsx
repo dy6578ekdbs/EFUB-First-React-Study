e90260634f03590031a7510fabb86b6f39893b29
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-05-06 23:14:52 +0900

    week6

---
+++ chapter_13/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+function ProfileCard(props) {
+  return (
+    <Card title="Hyeonsu" backgroundColor="#4ea04e">
+      <p> 안녕하세요, 김현수입니다.</p>
+      <p>저는 리액트를 사용해서 개발하고 있습니다. </p>
+    </Card>
+  );
+}
+export default ProfileCard;
